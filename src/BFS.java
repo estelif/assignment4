b76ae40e3987888872436527890769731b3a0523
estelif <230682@astanait.edu.kz>
2024-05-30 00:03:09 +0500

    BreadthFirstSearch methods

---
+++ src/BFS.java
@@ -0,0 +1,31 @@
+/*BFS class implements the Breadth-First Search algorithm
+for traversing or searching through a graph.
+ */
+
+
+import java.util.*;
+
+public class BFS<Vertex> extends Search <Vertex> {
+    public BFS(MyGraph<Vertex> graph, Vertex source) {
+        super(source);
+        bfs(graph, source);
+    }
+
+    private void bfs(MyGraph<Vertex> graph, Vertex current) {
+        marked.add(current);
+        Queue<Vertex> queue = new LinkedList<>();
+        queue.add(current);
+
+        while (!queue.isEmpty()) {
+            Vertex v = queue.poll();
+
+            for (Vertex neighbor : graph.adjacencyList(v)) {
+                if (!marked.contains(neighbor)) {
+                    marked.add(neighbor);
+                    edgeTo.put(neighbor, v);
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
+}
